80ca1fad27694b4964784def6340fd0978f22d2b
luisartur8 <luisartur8@hotmail.com>
2025-01-26 23:24:42 -0300

    merge

--- src/components/CarregaPlanilha/TableCell/dblClickHandler.js
+++
@@ -1,9 +0,0 @@
-export function dblClickHandler(setEditable, cellRef, moverCursorParaFinal) {
-    setEditable(true);
-    setTimeout(() => {
-        if (cellRef.current) {
-            cellRef.current.focus();
-            moverCursorParaFinal(cellRef.current);
-        }
-    }, 0);
-}
